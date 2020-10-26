58f22a40e6838c804a7d4963e0496350a6d81a8f
deton-esc5 <dmeverton@gmail.com>
2020-10-26 18:25:12 -0300

    Subindo as páginas de texto que faltavam e alterada a página de quemsomos.ejs para deixar no padrão das outras

--- ProjetoIntegradorDH/routes/index.js
+++ ProjetoIntegradorDH/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
+router.get('/seguranca', function(req, res, next) {
+  res.render('seguranca_privacidade');
+});
+
 module.exports = router;
