831780b3d3adf3b524239757f81a4bbf5d0a139a
Vinicius Callegari <callegari18@hotmail.com>
2020-10-26 22:02:54 -0300

    adiçao de rotas

--- ProjetoIntegradorDH/routes/index.js
+++ ProjetoIntegradorDH/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
+router.get('/contato', function(req, res, next) {
+  res.render('contato');
+});
+
+router.get('/cadastro', function(req, res, next) {
+  res.render('cadastro');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/parceiros', function(req, res, next) {
+  res.render('parceiros');
+});
+router.get('/sobrenos', function(req, res, next) {
+  res.render('sobrenos');
+});
+
 module.exports = router;
