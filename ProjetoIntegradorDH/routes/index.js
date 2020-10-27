4a6db41d338455caa11d53f0241cb1f30bfb2f96
Vinicius Callegari <callegari18@hotmail.com>
2020-10-27 09:01:59 -0300

    corrigindo rota seguranca_privacidade

--- ProjetoIntegradorDH/routes/index.js
+++ ProjetoIntegradorDH/routes/index.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home');
+  res.render('home')
 });
 
-<<<<<<< HEAD
+
 router.get('/contato', function(req, res, next) {
-  res.render('contato');
+  res.render('contato')
 });
 
 router.get('/cadastro', function(req, res, next) {
-  res.render('cadastro');
+  res.render('cadastro')
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  res.render('login')
 });
 
 router.get('/parceiros', function(req, res, next) {
-  res.render('parceiros');
+  res.render('parceiros')
 });
 router.get('/sobrenos', function(req, res, next) {
-  res.render('sobrenos');
-=======
+  res.render('sobrenos')
+});
+
 router.get('/seguranca', function(req, res, next) {
-  res.render('seguranca_privacidade');
->>>>>>> 58f22a40e6838c804a7d4963e0496350a6d81a8f
+  res.render('seguranca_privacidade')
 });
 
 module.exports = router;
