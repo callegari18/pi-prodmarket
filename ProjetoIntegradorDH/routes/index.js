fe42a376c07ae2b34f923a446a08eca7f6801d23
Vinicius Callegari <callegari18@hotmail.com>
2020-10-28 11:13:01 -0300

    criação de 2 formulários cadastro usuario e produto

--- ProjetoIntegradorDH/routes/index.js
+++ ProjetoIntegradorDH/routes/index.js
@@ -6,22 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('home')
 });
 
-
 router.get('/contato', function(req, res, next) {
   res.render('contato')
 });
 
-router.get('/cadastro', function(req, res, next) {
-  res.render('cadastro')
+router.get('/cadastro_usuario', function(req, res, next) {
+  res.render('cadastro_usuario')
+});
+router.post('/cadastro_usuario', function(req, res, next) {
+  res.render('cadastro_usuario')
+});
+
+router.get('/cadastro_produto', function(req, res, next) {
+  res.render('cadastro_produto')
+});
+router.post('/cadastro_produto', function(req, res, next) {
+  res.render('cadastro_produto')
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login')
 });
 
+
 router.get('/parceiros', function(req, res, next) {
   res.render('parceiros')
 });
+
 router.get('/sobrenos', function(req, res, next) {
   res.render('sobrenos')
 });
