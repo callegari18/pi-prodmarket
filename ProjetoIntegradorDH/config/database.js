13e67f41c03a4f819d7865690890fcee995ca8c6
Vinicius Callegari <callegari18@hotmail.com>
2020-11-26 10:11:15 -0300

    reposicionando pasta models para raiz

--- ProjetoIntegradorDH/config/database.js
+++ ProjetoIntegradorDH/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: 'root',
-  password: 'root',
-  database: 'crud_sequelize',
+  password: '0709',
+  database: 'cadastro',
   host: '127.0.0.1',
   dialect: 'mysql',
 }
