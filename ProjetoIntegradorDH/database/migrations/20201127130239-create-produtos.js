3280eb0e5a589ee6d2e8c885273c2b1c5697f8be
Pedro Queiroz <pedrov-queiroz@hotmail.com>
2020-11-27 10:26:51 -0300

    tabelas produtos,pedidos e pedidosprodutos

---
+++ ProjetoIntegradorDH/database/migrations/20201127130239-create-produtos.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Produtos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoria: {
+        type: Sequelize.STRING
+      },
+      qualificação: {
+        type: Sequelize.STRING
+      },
+      quantidade: {
+        type: Sequelize.STRING
+      },
+      valor: {
+        type: Sequelize.STRING
+      },
+      descricao: {
+        type: Sequelize.STRING
+      },
+      imagem: {
+        type: Sequelize.STRING
+      },
+      codigo_produto: {
+        type: Sequelize.STRING
+      },
+      Usuarios_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Produtos');
+  }
+};
