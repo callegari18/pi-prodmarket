c70844d035ee2422065110aa1ccb1927daba7019
Pedro Queiroz <pedrov-queiroz@hotmail.com>
2020-11-28 18:20:29 -0300

    atualização

--- ProjetoIntegradorDH/config/database.js
+++ ProjetoIntegradorDH/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: 'root',
-  password: '0709',
-  database: 'cadastro',
+  password: '19950729196373',
+  database: 'prodmarket',
   host: '127.0.0.1',
   dialect: 'mysql',
 }
