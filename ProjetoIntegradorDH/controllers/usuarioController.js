dcf311e933b48e4a76ac1c16a0cc578957dec750
Vinicius Callegari <callegari18@hotmail.com>
2020-11-29 23:01:09 -0300

    atualizando

--- ProjetoIntegradorDH/controllers/usuarioController.js
+++ ProjetoIntegradorDH/controllers/usuarioController.js
@@ -8,17 +8,17 @@ create:(req,res) => {
 },
 
 // salvar:async(req, res, next) => {
-//     //let form=(req.body)
+//     let form=(req.body)
 //     console.log("dados:",req.body)
 // }
 
-/salvar: async (req, res) => {
+/*salvar: async (req, res) => {
     // Pegar os dados da requisição
     // Jogar os dados no banco
     // Redirecionar
     const dados = req.body;
     console.log("DADOS", dados);
-   const result = await usuarios.create(dados);/
+   const result = await usuarios.create(dados);*/
 
 store: async (req, res)=>{
     const { tipo
