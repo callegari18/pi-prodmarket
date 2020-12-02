771e038e1271c05ab7d5db55082c8867ae793f12
Pedro Queiroz <pedrov-queiroz@hotmail.com>
2020-12-02 19:47:36 -0300

    html cadastros atualizados

--- ProjetoIntegradorDH/models/enderecos.js
+++ ProjetoIntegradorDH/models/enderecos.js
@@ -9,8 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+   static associate(models) {
       // define association here
+      //this.belongsTo(models.Enderecos, { 
+        //foreignHey: 'Usuarios_id' , as: "usuarios" })
     }
   };
   Enderecos.init({
