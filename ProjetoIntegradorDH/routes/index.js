96e752f490c444c0fa2b385b209ab1a83597e963
Vinicius Callegari <callegari18@hotmail.com>
2020-12-04 22:43:00 -0300

    forEach home

--- ProjetoIntegradorDH/routes/index.js
+++ ProjetoIntegradorDH/routes/index.js
@@ -6,9 +6,7 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home')
-});
+router.get('/', produtoController.homeCard);
 
 router.get('/contato', function(req, res, next) {
   res.render('contato')
